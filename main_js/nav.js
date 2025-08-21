66e0f65c3a1cb9d73d757d96dfbb6658ac5f647f
rlaskarb <rlaskarb20@gmail.com>
2025-08-21 21:51:44 +0900

    8/21

--- main_js/nav.js
+++ main_js/nav.js
@@ -1,5 +1,32 @@
 $(document).ready(function () {
-    let openNav = false;
+    const $header = $('#headerArea');
+    const $gnb = $('#gnb');
+    const $blindBox = $('.blind_box');
 
-    $("")
+
+    $('.menu_ham , blind_box').click(function (e) {
+        e.preventDefault();
+
+        $header.toggleClass('mn_open');
+        if ($header.hasClass('mn_open')) {
+            $blindBox.show();
+            $gnb.css('height', $(document).height());
+        } else {
+            $blindBox.hide();
+        }
+    })
+
+    $('.main_menu h3 a').click(function (e) {
+        e.preventDefault();
+        const $clickLi = $(this).closest('li');
+
+        if ($clickLi.hasClass('active')){
+            $clickLi.removeClass('active');
+        } else {
+            $('#gnb .main_menu li').removeClass('active');
+            $clickLi.addClass('active');
+            
+        }
+    })
+
 });
