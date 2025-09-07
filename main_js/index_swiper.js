cad1d5a071d4ba97b9970740fb46ef8c7784f2de
rlaskarb <rlaskarb20@gmail.com>
2025-09-07 21:50:42 +0900

    9/7

--- main_js/index_swiper.js
+++ main_js/index_swiper.js
@@ -1,51 +1,56 @@
 $(document).ready(function () {
-    visualSwiper = new Swiper(".visual_swiper", {
-        slidesPerView: 1,
-        spaceBetween: 0,
-        loop: true,
+	visualSwiper = new Swiper(".visual_swiper", {
+		slidesPerView: 1,
+		spaceBetween: 0,
+		loop: true,
 
-        autoplay: {
-            delay: 3500,
-            disableOnInteraction: false,
-        },
+		autoplay: {
+			delay: 3500,
+			disableOnInteraction: false,
+		},
 
-        pagination: {
-            el: ".visual_swiper .swiper-pagination",
-            clickable: true,
-        },
+		pagination: {
+			el: ".visual_swiper .swiper-pagination",
+			clickable: true,
+		},
 
-        on: {
-            slideChangeTransitionStart: function () {
-                $(".visual_text dt, .visual_text dd").css({
-                    opacity: 0,
-                    transform: "translateY(30px)",
-                });
-            },
-            slideChangeTransitionEnd: function () {
-                $(".swiper-slide-active .visual_text dt, .swiper-slide-active .visual_text dd").css({
-                    opacity: 1,
-                    transform: "translateY(10px)",
-                });
-            },
-        },
-    });
+		on: {
+			slideChangeTransitionStart: function () {
+				$(".visual_text dt, .visual_text dd").css({
+					opacity: 0,
+					transform: "translateY(30px)",
+				});
+			},
+			slideChangeTransitionEnd: function () {
+				$(
+					".swiper-slide-active .visual_text dt, .swiper-slide-active .visual_text dd"
+				).css({
+					opacity: 1,
+					transform: "translateY(10px)",
+				});
+			},
+		},
+	});
 
-    // 창업안내 스와이퍼
-    visualSwiper = new Swiper(".start_up_guide_swiper", {
-        effect: "cube",
-        cubeEffect: {
-            shadow: true,
-            slideShadows: true,
-            shadowOffset: 20,
-            shadowScale: 0.94,
-        },
-        slidesPerView: 1,
-        spaceBetween: 0,
-        loop: true,
-
-        pagination: {
-            el: ".start_up_guide_swiper .swiper-pagination",
-            clickable: true,
-        },
-    });
+	// 창업안내 스와이퍼
+	visualSwiper = new Swiper(".start_up_guide_swiper", {
+		effect: "creative",
+		creativeEffect: {
+			prev: {
+				shadow: true,
+				translate: ["-120%", 0, -500],
+			},
+			next: {
+				shadow: true,
+				translate: ["120%", 0, -500],
+			},
+		},
+		slidesPerView: 1,
+		spaceBetween: 0,
+		loop: true,
+		pagination: {
+			el: ".start_up_guide_swiper .swiper-pagination",
+			clickable: true,
+		},
+	});
 });
